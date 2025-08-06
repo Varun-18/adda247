d178dfe6fc0bef34273356828ed2050e629e2d20
Varun-18 <varunshah735@gmail.com>
2025-08-07 01:17:39 +0530

    deployment on render

--- api/index.ts
+++ api/index.ts
@@ -5,12 +5,10 @@ dotenv.config();
 
 const port = process.env.PORT || 5000;
 
-if (process.env.MODE === 'dev') {
-  app.listen(port, () => {
-    console.log(
-      `Server is running on ${process.env.MODE === 'prod' ? 'on vercel' : `http://localhost:${port}`}`
-    );
-  });
-}
+app.listen(port, () => {
+  console.log(
+    `Server is running on ${process.env.MODE === 'prod' ? 'on vercel' : `http://localhost:${port}`}`
+  );
+});
 
 export default app;
