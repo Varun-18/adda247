289eda1ba428c98a42197d129c52068668010bc3
Varun-18 <varunshah735@gmail.com>
2025-09-01 23:43:25 +0530

    domain changes v1

--- api/features/users/handlers/login.handler.ts
+++ api/features/users/handlers/login.handler.ts
@@ -47,7 +47,7 @@ export const login = async (req: Request, res: Response) => {
         maxAge: 3600000,
         sameSite: 'none',
         path: '/',
-        domain: 'https://adda247-scheduler.vercel.app',
+        domain: '.vercel.app',
       }
     );
   } catch (error) {
