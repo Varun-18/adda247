382f0c8c23b390bb618c41f62726dd2d81a6adc2
Varun-18 <varunshah735@gmail.com>
2025-09-01 23:28:33 +0530

    cookie domain added

--- api/features/users/handlers/login.handler.ts
+++ api/features/users/handlers/login.handler.ts
@@ -47,6 +47,7 @@ export const login = async (req: Request, res: Response) => {
         maxAge: 3600000,
         sameSite: 'none',
         path: '/',
+        domain: 'https://adda247-0u72.onrender.com',
       }
     );
   } catch (error) {
